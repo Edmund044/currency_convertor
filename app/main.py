0e0290fdd883df76a2db6b5191c300218a7aa4b4
EdmundEngineer <97869278+EdmundEngineer@users.noreply.github.com>
2022-06-15 08:56:27 +0300

    fix():changed the docker compose app

--- app/main.py
+++ app/main.py
@@ -34,6 +34,68 @@ def signin():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route("/handle-signup", methods=["POST"])
+def handle_signup():
+   if request.method == "POST":
+        first_name = request.form["first_name"]
+        second_name = request.form["second_name"]
+        currency = request.form["currency"]
+        email =  request.form["email"]
+        picture = request.form["picture"]
+        password =  request.form["password"]
+        confirm_password = request.form["confirm_password"]
+
+   return render_template("dashboard.html")
+
+@app.route("/handle-signin", methods=["POST"])
+def handle_signin():
+   if request.method == "POST":
+        email =  request.form["email"]
+        password =  request.form["password"] 
+
+   return render_template("dashboard.html")  
+
+@app.route("/get_transaction", methods=["POST"])
+def get_transaction():
+   if request.method == "POST":
+        email =  request.form["email"]
+        password =  request.form["password"] 
+
+   return render_template("dashboard.html")    
+
+
+@app.route("/handle-profile-update", methods=["POST"])
+def handle_profile_update():
+   if request.method == "POST":
+        first_name = request.form["first_name"]
+        second_name = request.form["second_name"]
+        currency = request.form["currency"]
+        email =  request.form["email"]
+        picture = request.form["picture"]
+        password =  request.form["password"]
+        confirm_password = request.form["confirm_password"]
+
+   return render_template("dashboard.html")
+
+@app.route("/handle-convert-currency", methods=["POST"])
+def handle_convert_currency():
+   if request.method == "POST":
+        amount1 =  request.form["amount1"]
+        currency1 = request.form["currency1"] 
+        amount2 =  request.form["amount2"]
+        currency2 = request.form["currency2"]       
+        
+   return render_template("dashboard.html") 
+
+@app.route("/handle-transfer-money", methods=["POST"])
+def handle_transfer_money():
+   if request.method == "POST":
+        amount_to_send =  request.form["amount_to_send"]
+        wallet_number = request.form["wallet_number"]      
+        
+   return render_template("dashboard.html")
+
+
 @app.route("/health")
 def health():
     return render_template("index.html")
