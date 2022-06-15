00f7d732209b6abae0b71362be50d19a100a63d9
EdmundEngineer <97869278+EdmundEngineer@users.noreply.github.com>
2022-06-15 13:44:48 +0300

    fix():replaced the old file structure in the main file

--- app/main.py
+++ app/main.py
@@ -1,36 +1,36 @@
 from flask import Flask, render_template, request
 import pymysql
 import secrets
-def create_app():
-  app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 
-  @app.route("/")
-  def index():
-      return render_template("signup.html")
+@app.route("/")
+def index():
+    return render_template("signup.html")
 
-  @app.route("/convert-currency")
-  def convert():
+@app.route("/convert-currency")
+def convert():
       return render_template("convert.html")
 
-  @app.route("/transfer-money")
-  def transfer():
+@app.route("/transfer-money")
+def transfer():
       return render_template("transfer-money.html")
 
-  @app.route("/profile")
-  def profile():
+@app.route("/profile")
+def profile():
       return render_template("profile.html")    
 
-  @app.route("/signin")
-  def signin():
+@app.route("/signin")
+def signin():
       return render_template("signin.html")
 
-  @app.route("/dashboard")
-  def dashboard():
+@app.route("/dashboard")
+def dashboard():
       return render_template("dashboard.html")
 
-  @app.route("/handle-signup", methods=["POST"])
-  def handle_signup():
+@app.route("/handle-signup", methods=["POST"])
+def handle_signup():
     if request.method == "POST":
           first_name = request.form["first_name"]
           second_name = request.form["second_name"]
@@ -42,16 +42,16 @@ def create_app():
 
     return render_template("dashboard.html")
 
-  @app.route("/handle-signin", methods=["POST"])
-  def handle_signin():
+@app.route("/handle-signin", methods=["POST"])
+def handle_signin():
     if request.method == "POST":
           email =  request.form["email"]
           password =  request.form["password"] 
 
     return render_template("dashboard.html")  
 
-  @app.route("/get_transaction", methods=["POST"])
-  def get_transaction():
+@app.route("/get_transaction", methods=["POST"])
+def get_transaction():
     if request.method == "POST":
           email =  request.form["email"]
           password =  request.form["password"] 
@@ -59,8 +59,8 @@ def create_app():
     return render_template("dashboard.html")    
 
 
-  @app.route("/handle-profile-update", methods=["POST"])
-  def handle_profile_update():
+@app.route("/handle-profile-update", methods=["POST"])
+def handle_profile_update():
     if request.method == "POST":
           first_name = request.form["first_name"]
           second_name = request.form["second_name"]
@@ -72,8 +72,8 @@ def create_app():
 
     return render_template("dashboard.html")
 
-  @app.route("/handle-convert-currency", methods=["POST"])
-  def handle_convert_currency():
+@app.route("/handle-convert-currency", methods=["POST"])
+def handle_convert_currency():
     if request.method == "POST":
           amount1 =  request.form["amount1"]
           currency1 = request.form["currency1"] 
@@ -82,8 +82,8 @@ def create_app():
           
     return render_template("dashboard.html") 
 
-  @app.route("/handle-transfer-money", methods=["POST"])
-  def handle_transfer_money():
+@app.route("/handle-transfer-money", methods=["POST"])
+def handle_transfer_money():
     if request.method == "POST":
           amount_to_send =  request.form["amount_to_send"]
           wallet_number = request.form["wallet_number"]      
@@ -91,12 +91,12 @@ def create_app():
     return render_template("dashboard.html")
 
 
-  @app.route("/health")
-  def health():
+@app.route("/health")
+def health():
       return render_template("index.html")
 
-  @app.route("/v1/")
-  def home():
+@app.route("/v1/")
+def home():
       return {
         "data":{
           "status":"ok",
@@ -104,8 +104,8 @@ def create_app():
         }
       }
 
-  @app.route("/v1/currencies")
-  def currencies():
+@app.route("/v1/currencies")
+def currencies():
       return {
         "data":{
           "status":"ok",
@@ -115,22 +115,21 @@ def create_app():
 
         
   # Invalid URL
-  @app.errorhandler(404)
-  def page_not_found(e):
+@app.errorhandler(404)
+def page_not_found(e):
     return {
     "status" : 404,
     "message" : "Page not found"
   }
 
   # Internal Server Error
-  @app.errorhandler(500)
-  def page_not_found(e):
+@app.errorhandler(500)
+def page_not_found(e):
     return {
     "status" : 500,
     "message" : "Internal server errror"
   }
 
-  if __name__ == "__main__":
+if __name__ == "__main__":
       app.run(host='0.0.0.0', debug=True)  
-
-  return app     
+   
