aaab2450167aeb9f82f275dea83b7d478844ee78
EdmundEngineer <97869278+EdmundEngineer@users.noreply.github.com>
2022-06-17 22:41:41 +0300

    attempted to add tests

--- app/use_cases.py
+++ app/use_cases.py
@@ -1,7 +1,16 @@
 import requests
 from flask import jsonify
 import json
+from flask import Flask, jsonify, render_template, request, url_for, redirect,session,flash
+import os
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = './static/uploads'
+app = Flask(__name__, template_folder='../templates',
+            static_folder='../static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER    
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 def make_request_to_convert_currency(amount1,  currency1, currency2):
     url = "https://api.apilayer.com/fixer/convert?to=" + \
@@ -18,3 +27,14 @@ def make_request_to_convert_currency(amount1,  currency1, currency2):
     res = json.loads(response.text)
 
     return res
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload_file(image):
+        if image and allowed_file(image.filename):
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return os.path.join(app.config['UPLOAD_FOLDER'], filename)
